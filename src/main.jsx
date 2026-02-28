63528c090818078f71da8bc675ea835525d5d85b
cktwi95 <cktwi95@gmail.com>
2026-03-01 00:48:39 +0100

    fix: ajout fonts Google + route siem-wazuh

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import PentestWebAWS from './pages/PentestWebAWS.jsx'
 import PentestAD from './pages/PentestAD.jsx'
+import SiemWazuh from './pages/SiemWazuh.jsx'
 import './index.css'
-
-function RedirectToProjets() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate('/');
-    setTimeout(() => {
-      document.getElementById('projets')?.scrollIntoView({ behavior: 'smooth' });
-    }, 200);
-  }, []);
-  return null;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/projets" element={<RedirectToProjets />} />
         <Route path="/projets/pentest-web-aws" element={<PentestWebAWS />} />
         <Route path="/projets/pentest-ad" element={<PentestAD />} />
+        <Route path="/projets/siem-wazuh" element={<SiemWazuh />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
