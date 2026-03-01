7258da82b56eb61ef4a1a27e3c7fd24ef2eeb5ed
cktwi95 <cktwi95@gmail.com>
2026-03-01 21:38:54 +0100

    redirect projets v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import App from './App.jsx'
 import PentestWebAWS from './pages/PentestWebAWS.jsx'
 import PentestAD from './pages/PentestAD.jsx'
@@ -10,11 +10,24 @@ import CspmQualys from './pages/CspmQualys.jsx'
 import OpenLdap from './pages/OpenLdap.jsx'
 import './index.css'
 
+function RedirectToProjets() {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    navigate('/');
+    setTimeout(() => {
+      document.getElementById('projets')?.scrollIntoView({ behavior: 'smooth' });
+    }, 200);
+  }, []);
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/projets" element={<RedirectToProjets />} />
+        <Route path="/projets/" element={<RedirectToProjets />} />
         <Route path="/projets/pentest-web-aws" element={<PentestWebAWS />} />
         <Route path="/projets/pentest-ad" element={<PentestAD />} />
         <Route path="/projets/siem-wazuh" element={<SiemWazuh />} />
